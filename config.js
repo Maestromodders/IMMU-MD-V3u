3470d18c7469b39450abfbbd43e62bedebc5c523
Maestromodders <fmode9584@gmail.com>
2026-05-20 15:29:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,26 +6,26 @@ require('dotenv').config();
 
 module.exports = {
   // ─── Identity ──────────────────────────────
-  botName: process.env.BOT_NAME || "IMMU MD V3",
+  botName: process.env.BOT_NAME || "CYBERPUNKBULLY",
   botVersion: "3.0.0",
 
   // ─── Branding ──────────────────────────────
-  botPic: process.env.BOT_PIC || "https://i.ibb.co/p6frQZNZ/Picsart-26-04-27-12-36-40-582.jpg",
-  botFooter: "Powered by IMMU MD V3",
+  botPic: process.env.BOT_PIC || "hhttps://files.catbox.moe/pvc7d6.jpg",
+  botFooter: "Powered by CYBERPUNKBULLY",
 
   // ─── Channel & Group (Auto-follow on first connect) ───
-  newsletterJid: "120363341506278064@newsletter",
-  newsletterName: "𝐈ᴍᴍυ 𝐌ᴅ",
+  newsletterJid: "120363404552894723@newsletter",
+  newsletterName: "CYBERPUNK-BULLY",
   newsletterUrl: "https://whatsapp.com/channel/0029Vaq4PRsD38CJKXzwmb42",
 
   // Auto-follow these channels on first connect
   autoFollowChannels: [
-    "120363341506278064@newsletter",
+    "120363404552894723@newsletter",
   ],
 
   // Auto-join these groups on first connect (invite codes)
   autoJoinGroups: [
-    "JQTH0GwURpjIJEzhpcFosO",
+    "EjvJRIKwQsAGm3WcZzrKIk",
   ],
 
   // ─── Bot Settings ──────────────────────────
@@ -35,7 +35,7 @@ module.exports = {
   port: parseInt(process.env.PORT) || 8000,
 
   // ─── Authentication ────────────────────────
-  sessionId: process.env.SESSION_ID || "",
+  sessionId: process.env.SESSION_ID || "IMMU-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRU9PUHJLMGFCNjYzUWViRVcvNG4rejlwbHFOU09NTGcveGZFd1NVRi9VVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRWVyaTBTb29oYXhtYTBxQzJKVFBPTGEyb0pvcWxRTlRYQzJmdk96alBrUT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTSHhLT3VRblNpL2VtVXRtV1hkTk1PamorT25MY0lUVWRQdkx6STByRVZJPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJZT29LVTNBZUg2SHRkY0FlUHd6Z3BCY3RSVjZncWhld1JOU1RkU2lyRWtNPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFEczVQRFRVaGFJWmJlT09qbFRvOWt4U0F2RlBCd0k4N1dNUjVHcTVKMmc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlpHbTZDbEZGeW5YOStZMFo3MG1tSjlXMFlUQ21jdVo1VllxMlhqNVZyQzg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkZPYlJLakRaYkh0Vmp5eHZjazAwMnNSOFphTCttMWNtc3V6YmVzMi9udz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidFdzdXFxd2ZxdGc0eXpVNWFrT1E4THlSblRaVWtBZW1pdVNxTU9NQWdTOD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJmOEZ3LzNhY1R4SlJxSjlhSDZOM3BibzQvMWhscTROLzREZ01rcVg1dGo2TXpiV2RTcFk5RmczNDQyUCs1dG54YnpaRzRwVTJaQmlDREVnOWdZT0RnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NjYsImFkdlNlY3JldEtleSI6IlJ2Z0lnMXU5dGRzVDRhTTR0WFUwVU5FVm1Obkw3ZloxZ1ZmQUw5Q3BGWVU9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjo4MTMsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjo4MTMsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sInJlZ2lzdGVyZWQiOnRydWUsInBhaXJpbmdDb2RlIjoiMlBCWk5TUDgiLCJtZSI6eyJpZCI6IjI1NDc4ODQwOTEwNTo2QHMud2hhdHNhcHAubmV0IiwibGlkIjoiMTk4NjE3MDIxMzA1MDA5OjZAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNLV1Z4OWNCRUszTnR0QUdHQVVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiI0alExZEkydE1FcFFzZ1VHaXo3bkhLdlBTZlRiMXFGYWswVjFWdGtJVzBZPSIsImFjY291bnRTaWduYXR1cmUiOiI3dkxMRnJ5aExvTVcza2xxM1VOSytRR1k0RFRVUXNxU0JkZGd1OHBsV3Q0UDc0NDlSVjM4cUJDNU1hcENOV2FSMzJRbmIyam9TQlVWTUt1Rmt5eTRBZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiMTREeDlrMHFCaTlvSU8yN1JOc3cwOXJ2RlAvYjZleWJDY1FONTlYejFFT0tJRjVRSHRMdUNWRGZHOStTMWVCZkptUkQ1OVZLNnhkQU1Sc3ZxU2JtQWc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIxOTg2MTcwMjEzMDUwMDk6NkBsaWQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZUkwTlhTTnJUQktVTElGQm9zKzV4eXJ6MG4wMjlhaFdwTkZkVmJaQ0Z0RyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FJSURRZ1MifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzc5Mjc5NTQxLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUdITiJ9",
   usePairCode: process.env.USE_PAIR_CODE !== "false",
 
   // ─── Auto Features (always on for Heroku) ─
